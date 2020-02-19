fcf06eb4bd0c3965a4bcbd55314279768f4581f8
Юрий <bomzheg@gmail.com>
2020-02-19 13:47:18 +0300

    Bugfix, auto generation schemas (#1)
    * some bugfixes
    
    * add script to generate schemas

--- __main__.py
+++ __main__.py
@@ -1,4 +1,3 @@
-#!sh-venv/bin/python
 from app.utils.cli import cli
 
 if __name__ == "__main__":
--- app/models/user_karma.py
+++ app/models/user_karma.py
@@ -2,6 +2,7 @@ from math import log10
 
 from tortoise import fields
 from tortoise.models import Model
+from loguru import logger
 
 from .chat import Chat
 from .user import User
@@ -11,24 +12,26 @@ class UserKarma(Model):
     uc_id = fields.IntField(pk=True)
     user: fields.ForeignKeyRelation[User] = fields.ForeignKeyField('models.User', related_name='karma')
     chat: fields.ForeignKeyRelation[Chat] = fields.ForeignKeyField('models.Chat', related_name='user_karma')
-    karma: fields.FloatField(default=0)
+    karma = fields.FloatField(default=1)
 
     class Meta:
         table = 'user_karma'
 
     def __str__(self):
-        rez = self.uc_id
+        rez = str(self.uc_id)
         return rez
 
     def __repr__(self):
         return str(self)
 
     async def up(self, user_changed: User):
+        await self.fetch_related('chat')
         power = await self.get_power(user_changed, self.chat)
         self.karma += power
         await self.save()
 
     async def down(self, user_changed: User):
+        await self.fetch_related('chat')
         power = await self.get_power(user_changed, self.chat)
         self.karma -= power
         await self.save()
@@ -36,8 +39,11 @@ class UserKarma(Model):
     @classmethod
     async def get_power(cls, user: User, chat: Chat) -> float:
         uk, _ = await cls.get_or_create(user=user, chat=chat)
-        if uk.karma < 0:
+        if uk.karma < 0.0:
             return 0
-        if uk.karma <= 1:
+        if uk.karma <= 1.0:
             return 1
         return log10(uk.karma + 1)
+    @property
+    def karma_round(self):
+        return round(self.karma, 2)
