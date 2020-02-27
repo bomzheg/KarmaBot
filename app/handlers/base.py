e917ab6b31721b1534e5118179567e3338fe0e22
Юрий <bomzheg@gmail.com>
2020-02-27 12:28:30 +0300

    add description for /start /help /about

--- app/handlers/base.py
+++ app/handlers/base.py
@@ -10,18 +10,26 @@ from app.models.user import User
 @dp.message_handler(CommandStart())
 async def cmd_start(message: types.Message, user: User):
     logger.info("User {user} start conversation with bot", user=message.from_user.id)
-    await message.answer('messages.START_MSG')
+    await message.answer(
+        "Бот для изменения кармы в группе, просто добавьте "
+        "в группу и плюсуйте друг другу в карму"
+    )
 
 
 @dp.message_handler(CommandHelp())
 async def cmd_help(message: types.Message):
     logger.info("User {user} read help in {chat}", user=message.from_user.id, chat=message.chat.id)
-    await message.reply('messages.HELP_MSG')
+    await message.reply(
+        "Плюсануть в карму можно начав сообщение с '+' 'спасибо' '👍'. "
+        "Минусануть:  '-' '👎'.\n"
+        "Чтобы выбрать пользователя - нужно ответить реплаем на сообщение пользователя "
+        "или упомянуть его через @ (работает даже если у пользователя нет username."
+    )
 
 
 @dp.message_handler(commands=["about"])
 async def cmd_about(message: types.Message):
-    await message.reply('messages.ABOUT_MSG')
+    await message.reply('Исходники по ссылке https://github.com/bomzheg/KarmaBot')
 
 
 @dp.message_handler(state='*', commands='cancel')
