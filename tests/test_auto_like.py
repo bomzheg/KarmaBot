6a2b1885afa9f997e5deb7c6bd56a3ebb1468060
Yuriy Chebyshev <cheb.yuriy@gmail.com>
2020-07-25 21:14:04 +0300

    add tests

---
+++ tests/test_auto_like.py
@@ -0,0 +1,45 @@
+import typing
+
+from .fixtures import get_from_user, generate_phrases_next_word, get_message_with_reply, \
+    get_message_with_text_mention, get_message_with_mention, get_next_word_parts
+
+
+from .common import plus_texts, minus_texts, punctuations, filter_check
+
+
+def test_reply():
+    user = get_from_user(321, "Kripke")
+    for text in [*plus_texts, *minus_texts]:
+        for phrase in generate_phrases_next_word(text, punctuations):
+            check_plus_reply(user, phrase)
+
+
+def check_plus_reply(user: dict, text_with_plus_trigger: str):
+    filter_rez = filter_check(get_message_with_reply(user, user, text_with_plus_trigger))
+    assert filter_rez == {}
+
+
+def test_plus_mention():
+    user = get_from_user(321, "Kripke")
+
+    for text in [*plus_texts, *minus_texts]:
+        for precursors_list in get_next_word_parts(text, punctuations):
+            check_plus_mention(user, precursors_list)
+
+
+def check_plus_mention(user: dict, text_precursors: typing.List[str]):
+    filter_rez = filter_check(get_message_with_mention(user, user, text_precursors))
+    assert filter_rez == {}
+
+
+def test_plus_text_mention():
+    user = get_from_user(321, first_name="Barry")
+
+    for text in [*plus_texts, *minus_texts]:
+        for precursors_list in get_next_word_parts(text, punctuations):
+            check_plus_text_mention(user, precursors_list)
+
+
+def check_plus_text_mention(user: dict, text_precursors: typing.List[str]):
+    filter_rez = filter_check(get_message_with_text_mention(user, user, text_precursors))
+    assert filter_rez == {}
---
+++ tests/test_correct_karma_filter.py
@@ -0,0 +1,66 @@
+import typing
+
+from .fixtures import get_from_user, generate_phrases_next_word, get_message_with_reply,\
+    get_message_with_text_mention, get_message_with_mention, get_next_word_parts
+
+from aiogram import types
+
+from .common import plus_texts, minus_texts, punctuations, filter_check
+
+
+def test_plus_reply():
+    target_user = get_from_user(666, "Sheldon")
+    author_user = get_from_user(13, "Leonard")
+    for text in plus_texts:
+        for phrase in generate_phrases_next_word(text, punctuations):
+            check_plus_reply(author_user, target_user, phrase)
+
+
+def check_plus_reply(author_user: dict, target_user: dict, text_with_plus_trigger: str):
+    filter_rez = filter_check(get_message_with_reply(author_user, target_user, text_with_plus_trigger))
+    assert filter_rez['karma']['user'] == types.User(**target_user)
+    assert filter_rez['karma']['karma_change'] == 1
+
+
+def test_minus_reply():
+    target_user = get_from_user(99, "Howard")
+    author_user = get_from_user(36, "Rajesh")
+
+    for text in minus_texts:
+        check_minus_reply(author_user, target_user, text)
+
+
+def check_minus_reply(author_user: dict, target_user: dict, text_with_minus_trigger: str):
+    filter_rez = filter_check(get_message_with_reply(author_user, target_user, text_with_minus_trigger))
+    assert filter_rez['karma']['user'] == types.User(**target_user)
+    assert filter_rez['karma']['karma_change'] == -1
+
+
+def test_plus_mention():
+    target_user = get_from_user(666, "Bernadette")
+    author_user = get_from_user(13, "Amy")
+
+    for text in plus_texts:
+        for precursors_list in get_next_word_parts(text, punctuations):
+            check_plus_mention(author_user, target_user, precursors_list)
+
+
+def check_plus_mention(author_user: dict, target_user: dict, text_precursors: typing.List[str]):
+    filter_rez = filter_check(get_message_with_mention(author_user, target_user, text_precursors))
+    assert filter_rez['karma']['user'].username == target_user["username"]
+    assert filter_rez['karma']['karma_change'] == 1
+
+
+def test_plus_text_mention():
+    target_user = get_from_user(37, first_name="Leslie")
+    author_user = get_from_user(50, "Stuart")
+
+    for text in plus_texts:
+        for precursors_list in get_next_word_parts(text, punctuations):
+            check_plus_text_mention(author_user, target_user, precursors_list)
+
+
+def check_plus_text_mention(author_user: dict, target_user: dict, text_precursors: typing.List[str]):
+    filter_rez = filter_check(get_message_with_text_mention(author_user, target_user, text_precursors))
+    assert filter_rez['karma']['user'] == types.User(**target_user)
+    assert filter_rez['karma']['karma_change'] == 1
