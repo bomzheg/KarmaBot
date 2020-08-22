8b22b705ed0ca019984c14cd10a96f1bf46f6598
Yuriy Chebyshev <bomzheg@gmail.com>
2020-08-22 18:55:12 +0300

    add button to cancel change karma
    and move logging to service pakage as possible

--- app/handlers/change_karma.py
+++ app/handlers/change_karma.py
@@ -1,14 +1,21 @@
+import asyncio
+import typing
+from contextlib import suppress
+
 from aiogram import types
 from aiogram.types import ContentType
+from aiogram.utils.exceptions import MessageToEditNotFound, MessageCantBeEdited
 from aiogram.utils.markdown import hbold
 from loguru import logger
 
 from app.misc import dp
+from app import config
 from app.models.chat import Chat
 from app.models.user import User
-from app.services.change_karma import change_karma
+from app.services.change_karma import change_karma, cancel_karma_change
 from app.utils.exceptions import SubZeroKarma
 from app.services.find_target_user import get_db_user_by_tg_user
+from . import keyboards as kb
 
 how_change = {
     +1: 'увеличили',
@@ -26,7 +33,7 @@ async def karma_change(message: types.Message, karma: dict, user: User, chat: Ch
     target_user = await get_db_user_by_tg_user(target)
 
     try:
-        uk, power = await change_karma(
+        uk, power, karma_event = await change_karma(
             target_user=target_user,
             chat=chat,
             user=user,
@@ -37,17 +44,29 @@ async def karma_change(message: types.Message, karma: dict, user: User, chat: Ch
         logger.info("user {user} try to change karma but have negative karma", user=user.tg_id)
         return await message.reply("У Вас слишком мало кармы для этого")
 
-    await message.reply(
+    msg = await message.reply(
         "Вы {how_change} карму {name} до {karma_new} ({power:+.2f})".format(
             how_change=how_change[karma['karma_change']],
             name=hbold(target_user.fullname),
             karma_new=hbold(uk.karma_round),
             power=power * karma['karma_change'],
         ),
-        disable_web_page_preview=True
+        disable_web_page_preview=True,
+        reply_markup=kb.get_kb_karma_cancel(user, karma_event)
     )
-    logger.info(
-        "user {user} change karma of {target_user}",
-        user=user.tg_id,
-        target_user=target_user.tg_id
-    )
+    asyncio.create_task(remove_kb_after_sleep(msg))
+
+
+async def remove_kb_after_sleep(message: types.Message):
+    await asyncio.sleep(config.TIME_TO_CANCEL_ACTIONS)
+    with suppress(MessageToEditNotFound, MessageCantBeEdited):
+        await message.edit_reply_markup()
+
+
+@dp.callback_query_handler(kb.cb_karma_cancel.filter())
+async def cancel_karma(callback_query: types.CallbackQuery, callback_data: typing.Dict[str, str]):
+    if int(callback_data['user_id']) != callback_query.from_user.id:
+        return await callback_query.answer("Эта кнопка не для вас", cache_time=3600)
+    await cancel_karma_change(callback_data['action_id'])
+    await callback_query.answer("Вы отменили изменение кармы", show_alert=True)
+    await callback_query.message.delete()
--- app/services/change_karma.py
+++ app/services/change_karma.py
@@ -19,12 +19,38 @@ async def change_karma(user: User, target_user: User, chat: Chat, how_change: fl
         user_changed=user,
         how_change=how_change
     )
-    await KarmaEvent(
+    ke = KarmaEvent(
         user_from=user,
         user_to=target_user,
         chat=chat,
         how_change=how_change,
-        how_match_change=power*abs(how_change),
+        how_change_absolute=power*how_change,
         comment=comment
-    ).save()
-    return uk, power
+    )
+    await ke.save()
+    logger.info(
+        "user {user} change karma of {target_user} in chat {chat}",
+        user=user.tg_id,
+        target_user=target_user.tg_id,
+        chat=chat.chat_id
+    )
+    return uk, power, ke
+
+
+async def cancel_karma_change(karma_event_id):
+    karma_event = await KarmaEvent.get(id_=karma_event_id)
+
+    # noinspection PyUnresolvedReferences
+    user_to_id = karma_event.user_to_id
+    # noinspection PyUnresolvedReferences
+    user_from_id = karma_event.user_from_id
+    # noinspection PyUnresolvedReferences
+    chat_id = karma_event.chat_id
+
+    user_karma = await UserKarma.get(chat_id=chat_id, user_id=user_to_id)
+    user_karma.karma -= karma_event.how_change_absolute
+    await user_karma.save(update_fields=['karma'])
+    await karma_event.delete()
+    logger.info(
+        "user {user} cancel change karma to user {target} in chat {chat}",
+        user=user_from_id, target=user_to_id, chat=chat_id)
