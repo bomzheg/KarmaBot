401153231d92ad83d337b0a130f7108aba82d436
Yuriy Chebyshev <bomzheg@gmail.com>
2020-09-04 09:43:06 +0300

    refactor erros_handler

--- app/handlers/errors.py
+++ app/handlers/errors.py
@@ -1,4 +1,5 @@
 from aiogram import types
+from aiogram.utils.exceptions import CantParseEntities
 from aiogram.utils.markdown import quote_html
 from loguru import logger
 
@@ -10,12 +11,16 @@ from app.misc import dp, bot
 async def errors_handler(update: types.Update, exception: Exception):
     try:
         raise exception
+    except CantParseEntities as e:
+        logger.error("Cause exception {e} in update {update}", e=e, update=update)
+
     except Exception as e:
         logger.exception("Cause exception {e} in update {update}", e=e, update=update)
-        await bot.send_message(
-            config.LOG_CHAT_ID,
-            f"Получено исключение {quote_html(e)}\n"
-            f"во время обработки апдейта {quote_html(update)}\n"
-            f"{quote_html(' '.join(e.args))}"
-        )
+
+    await bot.send_message(
+        config.LOG_CHAT_ID,
+        f"Получено исключение {quote_html(exception)}\n"
+        f"во время обработки апдейта {quote_html(update)}\n"
+        f"{quote_html(exception.args)}"
+    )
     return True
