aa63488587d351b5e51eacbb7aee8bb930fe2c8f
Yuriy Chebyshev <cheb.yuriy@gmail.com>
2020-10-13 23:40:33 +0300

    migrate from time to datetime, fix bug with not exactly same time to handler and wrong throttle

--- app/services/adaptive_trottle.py
+++ app/services/adaptive_trottle.py
@@ -1,8 +1,9 @@
 import asyncio
 import functools
 import typing
-from time import time
+from datetime import datetime, timedelta
 
+from aiogram import types
 from aiogram.utils.exceptions import Throttled
 from loguru import logger
 
@@ -21,6 +22,8 @@ class AdaptiveThrottle:
             key: str = None,
             on_throttled: typing.Optional[typing.Callable] = None
     ):
+        rate = timedelta(seconds=rate)
+
         def decorator(func):
             current_key = key if key is not None else func.__name__
 
@@ -29,8 +32,9 @@ class AdaptiveThrottle:
                 chat: Chat = kwargs['chat']
                 user: User = kwargs['user']
                 target: User = kwargs['target']
+                message: types.Message = args[0]
 
-                if self.check_time_throttle(rate, current_key, chat.chat_id, user.tg_id, target.tg_id):
+                if self.check_time_throttle(message.date, rate, current_key, chat.chat_id, user.tg_id, target.tg_id):
                     return await func(*args, **kwargs)
                 else:
                     await process_on_throttled(on_throttled, current_key, rate, *args, **kwargs)
@@ -38,26 +42,33 @@ class AdaptiveThrottle:
             return wrapped
         return decorator
 
-    def check_time_throttle(self, rate: int, key: str, chat_id: int, user_id: int, target_user_id: int):
+    def check_time_throttle(
+            self,
+            call_at: datetime,
+            rate: int,
+            key: str,
+            chat_id: int,
+            user_id: int,
+            target_user_id: int
+    ):
         logger.debug(
             "check throttle time for chat {chat_id}, user {user_id}, target {target_user_id} and key {key}",
             chat_id=chat_id, user_id=user_id, target_user_id=target_user_id, key=key,
         )
 
-        now = time()
         bucket = self.get_bucket(chat_id, user_id, target_user_id)
 
         try:
             last = bucket[key]
         except KeyError:
-            logger.debug("there is no delta", )
+            logger.debug("there is no last call", )
             return True
         finally:
-            bucket[key] = now
+            bucket[key] = call_at
 
         self.set_bucket(chat_id, user_id, target_user_id, bucket)
-        logger.debug("delta is {delta}", delta=now - last)
-        return now - last > rate
+        logger.debug("delta is {delta}", delta=call_at - last)
+        return call_at - last > rate
 
     def get_bucket(self, chat_id: int, user_id: int, target_user_id: int):
         return self.last_calls.setdefault(chat_id, {}).setdefault(user_id, {}).setdefault(target_user_id, {})
