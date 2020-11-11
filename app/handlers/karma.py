edd8ed2a56dafd398fe6f7172151c791bd8b2838
Yuriy Chebyshev <cheb.yuriy@gmail.com>
2020-11-11 22:27:13 +0300

    split logic in private and group chats

--- app/handlers/karma.py
+++ app/handlers/karma.py
@@ -10,18 +10,27 @@ from app.models.user_karma import UserKarma
 from app.services.karma_top import get_karma_top
 
 
-@dp.message_handler(commands=["top"], commands_prefix='!')
-@dp.throttled(rate=60 * 5)
-async def get_top(message: types.Message, chat: Chat, user: User):
+@dp.message_handler(commands=["top"], commands_prefix='!', chat_type=types.ChatType.PRIVATE)
+@dp.throttled(rate=2)
+async def get_top_from_private(message: types.Message, chat: Chat, user: User):
     parts = message.text.split(maxsplit=1)
+    logger.info("user {user} ask top karma of chat {chat}", user=user.tg_id, chat=chat.chat_id)
     if len(parts) > 1:
         chat = await Chat.get(chat_id=int(parts[1]))
-    elif ChatType.is_private(message):
+    else:
         return await message.reply(
             "Эту команду можно использовать только в группах "
             "или с указанием id нужного чата, например:"
             "\n" + hpre("!top -1001399056118")
         )
+    text = await get_karma_top(chat, user)
+
+    await message.reply(text, disable_web_page_preview=True)
+
+
+@dp.message_handler(commands=["top"], commands_prefix='!')
+@dp.throttled(rate=60 * 5)
+async def get_top(message: types.Message, chat: Chat, user: User):
     logger.info("user {user} ask top karma of chat {chat}", user=user.tg_id, chat=chat.chat_id)
     text = await get_karma_top(chat, user)
 
