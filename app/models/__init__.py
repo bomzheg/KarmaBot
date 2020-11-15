d3cec8ab048643a96219dbfcf8054fd61077d46f
Yuriy Chebyshev <cheb.yuriy@gmail.com>
2020-11-15 14:04:32 +0300

    change order of import (if it was important for aerich (not was))

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,8 +1,8 @@
-from app.models.chat import Chat, ChatType
 from app.models.user import User
+from app.models.chat import Chat, ChatType
 from app.models.user_karma import UserKarma
 from app.models.karma_actions import KarmaEvent
 from app.models.moderator_actions import ModeratorEvent
 
-__all__ = [Chat, ChatType, User, UserKarma, KarmaEvent]
+__all__ = [User, Chat, ChatType, UserKarma, KarmaEvent, ModeratorEvent]
 
