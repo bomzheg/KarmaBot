cd700921d800713cffe0540adb5784eb9ac22674
Yuriy Chebyshev <ychebyshev@it.ru>
2021-03-17 21:55:15 +0300

    fix incorrect sql

--- migrations/03_add_settings.sql
+++ migrations/03_add_settings.sql
@@ -2,6 +2,5 @@ CREATE TABLE chat_settings (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     chat_id BIGINT,
     karmic_restrictions BOOLEAN DEFAULT false,
-    CONSTRAINT chat_settings_PK PRIMARY KEY (id),
     CONSTRAINT chat_settings_FK FOREIGN KEY (chat_id) REFERENCES chats(chat_id)
 );
