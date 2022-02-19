adf1350d1116db1268be9ab76bf7658a4e7e2c75
jorektheglitch <lozcontrariuz@yandex.ru>
2022-02-19 17:37:08 +0300

    Remove aiohttp_autoreload

--- app/utils/cli.py
+++ app/utils/cli.py
@@ -1,16 +1,10 @@
 # partially from https://github.com/aiogram/bot
 import argparse
-import functools
 
 from app.utils.log import Logger
 
 import app
-from app.models.config import WebhookConfig, Config
-
-try:
-    import aiohttp_autoreload
-except ImportError:
-    aiohttp_autoreload = None
+from app.models.config import Config
 
 
 logger = Logger(__name__)
@@ -25,8 +19,6 @@ def create_parser():
     arg_parser = argparse.ArgumentParser(prog=app.__application_name__, description=PROGRAM_DESC, epilog=PROGRAM_EP)
     arg_parser.add_argument('-p', '--polling', action='store_const', const=True,
                             help="Run tg bot with polling. Default use WebHook")
-    arg_parser.add_argument('-a', '--autoreload', action='store_const', const=True,
-                            help="Reload application on file changes")
     arg_parser.add_argument('-s', '--skip-updates', action='store_const', const=True,
                             help="Skip pending updates")
     return arg_parser
@@ -34,47 +26,18 @@ def create_parser():
 
 def cli(config: Config):
 
-    def auto_reload_mixin(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            if namespace.autoreload and aiohttp_autoreload:
-                logger.warning(
-                    "Application started in live-reload mode. Please disable it in production!"
-                )
-                aiohttp_autoreload.start()
-            elif namespace.autoreload and not aiohttp_autoreload:
-                logger.warning("`aiohttp_autoreload` is not installed.", err=True)
-            return func(*args, **kwargs)
-
-        return wrapper
-
-    @auto_reload_mixin
-    def polling(skip_updates: bool):
-        """
-        Start application
-        """
-        from app.utils.executor import runner
-        logger.info("starting polling...")
-
-        runner.skip_updates = skip_updates
-        runner.start_polling(reset_webhook=True)
-
-    @auto_reload_mixin
-    def webhook(webhook_config: WebhookConfig):
-        """
-        Run application in webhook mode
-        """
-        from app.utils.executor import runner
-        logger.info("starting webhook...")
-        runner.start_webhook(**webhook_config.listener_kwargs)
-
     parser = create_parser()
     namespace = parser.parse_args()
 
     from app import misc
+    from app.utils.executor import runner
 
     misc.setup(config)
     if namespace.polling:
-        polling(namespace.skip_updates)
+        logger.info("starting polling...")
+
+        runner.skip_updates = namespace.skip_updates
+        runner.start_polling(reset_webhook=True)
     else:
-        webhook(config.webhook)
+        logger.info("starting webhook...")
+        runner.start_webhook(**config.webhook.listener_kwargs)
