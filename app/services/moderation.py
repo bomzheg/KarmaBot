b55d5a692f45464864c6cec137d1b9de99efc3a6
Yuriy Chebyshev <ychebyshev@it.ru>
2022-05-11 23:45:54 +0300

    fix ChatMemberAdministrator

--- app/services/moderation.py
+++ app/services/moderation.py
@@ -2,6 +2,7 @@ import typing
 from datetime import timedelta
 
 from aiogram import Bot
+from aiogram.types import ChatMemberStatus
 from aiogram.utils.exceptions import BadRequest
 
 from app.config import moderation
@@ -124,7 +125,7 @@ def get_duration(text: str):
 
 async def user_has_now_ro(user: User, chat: Chat, bot: Bot):
     chat_member = await bot.get_chat_member(chat_id=chat.chat_id, user_id=user.tg_id)
-    return chat_member.can_send_messages is False
+    return chat_member.status in (ChatMemberStatus.BANNED, ChatMemberStatus.KICKED, ChatMemberStatus.RESTRICTED,)
 
 
 async def auto_restrict(target: User, chat: Chat, bot: Bot, using_db=None) -> typing.Tuple[int, ModeratorEvent]:
