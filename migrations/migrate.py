84380e26df5988395997cc851afd18b4d99ae763
Yuriy Chebyshev <bomzheg@gmail.com>
2023-11-23 10:23:57 +0300

    fixed migrations runner

--- migrations/migrate.py
+++ migrations/migrate.py
@@ -4,7 +4,6 @@ from pathlib import Path
 
 from app.config import load_config
 
-
 db_config = load_config().db
 
 with (Path(__file__).parent / sys.argv[1]).open() as f:
@@ -12,4 +11,4 @@ with (Path(__file__).parent / sys.argv[1]).open() as f:
 
 with sqlite3.connect(db_config.db_path) as conn:
     cur = conn.cursor()
-    cur.execute(sql)
+    cur.executescript(sql)
