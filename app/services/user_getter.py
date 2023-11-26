ec93e649e8f61cebe6955ea7d9d11cca26d9c557
Lamroy95 <aargh95@gmail.com>
2023-11-26 19:04:18 +0700

    Remove unused code

--- app/services/user_getter.py
+++ app/services/user_getter.py
@@ -1,10 +1,9 @@
 import asyncio
-import typing
 
 import pyrogram
 from aiogram.types import User
 from pyrogram import Client
-from pyrogram.errors import FloodWait, RPCError, UsernameNotOccupied
+from pyrogram.errors import FloodWait, UsernameNotOccupied
 
 from app.models.config import TgClientConfig
 from app.services.restrict_call import RestrictCall
@@ -31,24 +30,6 @@ class UserGetter:
             patched = self.restrict(getattr(self._client_api_bot, method))
             setattr(self._client_api_bot, method, patched)
 
-    async def get_user(
-        self, username: str = None, fullname: str = None, chat_id: int = None
-    ) -> User:
-        async def try_by_name() -> typing.Optional[User]:
-            try:
-                return await self.get_user_by_fullname(chat_id, fullname)
-            except (IndexError, RPCError):
-                return None
-
-        if username is not None:
-            try:
-                user_tg = await self.get_user_by_username(username)
-            except RPCError:
-                user_tg = await try_by_name()
-        else:
-            user_tg = await try_by_name()
-        return user_tg
-
     async def get_user_by_username(self, username: str) -> User:
         try:
             logger.info("get user of username {username}", username=username)
@@ -64,29 +45,6 @@ class UserGetter:
                 "Username resolver encountered flood error. Waited for %s", e.value
             )
 
-        return self.get_aiogram_user_by_pyrogram(user)
-
-    async def get_user_by_fullname(self, chat_id: int, fullname: str) -> User:
-        try:
-            logger.info("get user of name {name}", name=fullname)
-            chat_members = await self._client_api_bot.get_chat_members(
-                chat_id=chat_id, query=fullname
-            )
-            logger.info(
-                "found: {users}",
-                users=[
-                    self.get_user_dict_for_log(chat_member.user)
-                    for chat_member in chat_members
-                ],
-            )
-            user = chat_members[0].user
-        except IndexError:
-            logger.info("name not found {name}", name=fullname)
-            raise
-        except FloodWait as e:
-            logger.error("Flood Wait {e}", e=e)
-            await asyncio.sleep(e.x)
-            raise IndexError
         return self.get_aiogram_user_by_pyrogram(user)
 
     @staticmethod
