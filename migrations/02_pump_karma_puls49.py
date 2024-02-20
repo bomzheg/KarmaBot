64ab68878bfe56950afdc0cd9c3152b76a878512
Desiders <abc_desi@proton.me>
2024-02-20 15:15:13 +0300

    Use linters for `tests` and `migrations`

--- migrations/02_pump_karma_puls49.py
+++ migrations/02_pump_karma_puls49.py
@@ -2,13 +2,14 @@ import sqlite3
 
 from app.config import load_config
 
-
 db_config = load_config().db
 
 
 with sqlite3.connect(db_config.db_path) as conn:
     cur = conn.cursor()
-    cur.execute("""
-        UPDATE user_karma 
+    cur.execute(
+        """
+        UPDATE user_karma
         SET karma = karma + 49;
-    """)
+    """
+    )
